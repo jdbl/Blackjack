7eb1f3fadf22750e35b0740c0f0901e872f362d0
Brown <Jerry.Brown@cognex.com>
2020-01-10 15:53:57 -0800

    Fixed ads.

---
+++ Assets/Scripts/Ads.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+public class Ads : MonoBehaviour
+{
+	string gameId = "3415107";
+	bool testMode = true;
+	string placementId = "StandardPlaying";
+	// Start is called before the first frame update
+	void Start()
+    {
+		Advertisement.Initialize(gameId, testMode);
+		StartCoroutine(ShowBannerWhenReady());
+	}
+
+	IEnumerator ShowBannerWhenReady()
+	{
+		while (!Advertisement.IsReady(placementId))
+		{
+			yield return new WaitForSeconds(0.5f);
+		}
+		Advertisement.Banner.SetPosition(BannerPosition.TOP_CENTER);
+		
+		Advertisement.Banner.Show(placementId);
+		
+	}
+}
